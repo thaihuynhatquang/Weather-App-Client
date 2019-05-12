c74e546109677206c6526b998cbcf21e24be95df
Nhat Quang <thaihuynhatquang@gmail.com>
2019-05-12 18:46:03 +0700

    almost done radar screen

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  Dimensions
+} from "react-native";
+import { ButtonGroup } from "react-native-elements";
 import { MapView } from "expo";
 import {
   BACKGROUND_COLOR,
@@ -7,32 +14,74 @@ import {
   TEXT_LARGE_SIZE
 } from "../utils/constant";
 export default class Location extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      layerType: "temp_new",
+      selectedRadarTypeIndex: 2
+    };
+    this.chooseRadarType = this.chooseRadarType.bind(this);
+  }
+
+  chooseRadarType(selectedRadarTypeIndex) {
+    this.setState({ selectedRadarTypeIndex });
+    switch (selectedRadarTypeIndex) {
+      case 0:
+        this.setState({ layerType: "clouds_new" });
+        return;
+      case 1:
+        this.setState({ layerType: "wind_new" });
+        return;
+      case 2:
+        this.setState({ layerType: "temp_new" });
+        return;
+      case 3:
+        this.setState({ layerType: "pressure_new" });
+        return;
+      default:
+        return;
+    }
+  }
+
   render() {
     const { latitude, longitude } = this.props;
+    const { layerType, selectedRadarTypeIndex } = this.state;
+    const buttons = ["Cloud", "Wind", "Temperature", "Pressure"];
     if (latitude) {
       return (
-        <MapView
-          showsUserLocation
-          showsMyLocationButton
-          showsCompass
-          loadingEnabled
-          showsScale
-          style={{ flex: 1 }}
-          initialRegion={{
-            latitude,
-            longitude,
-            latitudeDelta: 180,
-            longitudeDelta: 180
-          }}
-        >
-          <MapView.UrlTile
-            urlTemplate={
-              "http://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=15db80d08721eec4d9cd77e4fcbfb163"
-            }
-            maximumZ={19}
-            flipY={false}
+        <View style={{ flex: 1 }}>
+          <MapView
+            showsUserLocation
+            showsCompass
+            showsScale
+            style={{ flex: 1 }}
+            initialRegion={{
+              latitude,
+              longitude,
+              latitudeDelta: 120,
+              longitudeDelta: 120
+            }}
+          >
+            <MapView.UrlTile
+              urlTemplate={
+                "http://tile.openweathermap.org/map/" +
+                layerType +
+                "/{z}/{x}/{y}.png?appid=15db80d08721eec4d9cd77e4fcbfb163"
+              }
+              maximumZ={19}
+              flipY={false}
+            />
+          </MapView>
+          <ButtonGroup
+            style={{
+              flex: 1
+            }}
+            onPress={this.chooseRadarType}
+            selectedIndex={selectedRadarTypeIndex}
+            buttons={buttons}
+            containerStyle={{ height: 30 }}
           />
-        </MapView>
+        </View>
       );
     }
     return (
